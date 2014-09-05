afd7edfcce4b6636be5c9bbdad09fc49ec0e89c0
Andreas Wolf <aw@foundata.net>
2014-09-05 22:09:15 +0200

    [FEATURE] Debugging proxy
    The debugging proxy is intended to sit between a debugger engine and an
    IDE to transmit packages back and forth, with the possiblity to
    intercept them. This is useful e.g. to learn about what is going on
    during a debugging session or to change data on-the-fly, e.g. for
    rewriting paths in breakpoints (if that is not supported by the IDE or
    necessary for other reasons).
    
    Currently, the proxy only dumps everything that comes through to the
    terminal. Further possibilites, e.g. listeners to handle the data, can
    be added in future commits.

---
+++ Scripts/DebugProxy.php
@@ -0,0 +1,12 @@
+<?php
+
+if (PHP_SAPI !== 'cli') {
+	echo(sprintf("This script was executed with a '%s' PHP binary. Make sure that you specified a CLI capable PHP binary in your \$PATH.", PHP_SAPI) . PHP_EOL);
+	exit(1);
+}
+
+require(__DIR__ . "/../Classes/AndreasWolf/DebuggerClient/Core/Bootstrap.php");
+\AndreasWolf\DebuggerClient\Core\Bootstrap::getInstance()->run();
+
+$application = new \AndreasWolf\DebuggerClient\Proxy\DebugProxy();
+$application->run();
