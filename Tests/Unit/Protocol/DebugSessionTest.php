1279e9aa78b9c76a5f263d037107c0ed55c2dc3f
Andreas Wolf <aw@foundata.net>
2014-09-20 16:09:34 +0200

    [TASK] Add a few unit tests for transaction handling

--- Tests/Unit/Protocol/DebugSessionTest.php
+++ Tests/Unit/Protocol/DebugSessionTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace AndreasWolf\DebuggerClient\Tests\Unit\Protocol;
 
+use AndreasWolf\DebuggerClient\Protocol\DebuggerCommand;
 use AndreasWolf\DebuggerClient\Protocol\DebugSession;
 use AndreasWolf\DebuggerClient\Tests\Unit\UnitTestCase;
 
@@ -18,6 +19,33 @@ class DebugSessionTest extends UnitTestCase {
 	public function newSessionIsNotInitialized() {
 		$session = new DebugSession();
 		$this->assertFalse($session->isInitialized());
+	}
+
+	/**
+	 * @test
+	 */
+	public function transactionsAreCorrectlyCreated() {
+		$session = new DebugSession();
+		/** @var DebuggerCommand $command */
+		$command = $this->getMock('AndreasWolf\DebuggerClient\Protocol\DebuggerCommand');
+
+		$transaction = $session->startTransaction($command);
+
+		$this->assertSame(0, $transaction->getId());
+	}
+
+	/**
+	 * @test
+	 */
+	public function consecutiveTransactionsHaveDifferentIds() {
+		$session = new DebugSession();
+		/** @var DebuggerCommand $command */
+		$command = $this->getMock('AndreasWolf\DebuggerClient\Protocol\DebuggerCommand');
+
+		$transactionA = $session->startTransaction($command);
+		$transactionB = $session->startTransaction($command);
+
+		$this->assertNotEquals($transactionA->getId(), $transactionB->getId());
 	}
 
 }
