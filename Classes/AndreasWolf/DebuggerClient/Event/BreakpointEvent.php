4cffd6db822cc59102637e42b1a26c3b3409b841
Andreas Wolf <aw@foundata.net>
2014-10-02 20:27:19 +0200

    [BUGFIX] Breakpoint event does not expose session

--- Classes/AndreasWolf/DebuggerClient/Event/BreakpointEvent.php
+++ Classes/AndreasWolf/DebuggerClient/Event/BreakpointEvent.php
@@ -1,6 +1,7 @@
 <?php
 namespace AndreasWolf\DebuggerClient\Event;
 use AndreasWolf\DebuggerClient\Protocol\Breakpoint\Breakpoint;
+use AndreasWolf\DebuggerClient\Session\DebugSession;
 use Symfony\Component\EventDispatcher\Event;
 
 
@@ -9,15 +10,16 @@ use Symfony\Component\EventDispatcher\Event;
  *
  * @author Andreas Wolf <aw@foundata.net>
  */
-class BreakpointEvent extends Event {
+class BreakpointEvent extends SessionEvent {
 
 	/**
 	 * @var Breakpoint
 	 */
 	protected $breakpoint;
 
-	public function __construct(Breakpoint $breakpoint) {
+	public function __construct(Breakpoint $breakpoint, DebugSession $session) {
 		$this->breakpoint = $breakpoint;
+		parent::__construct($session);
 	}
 
 	/**
